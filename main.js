7bca5d06bddaea04132b135129f720291f3c02aa
Fujita Ayato <tenhou.0126@gmail.com>
2024-11-06 15:29:33 +0900

    test.mac

--- main.js
+++ main.js
@@ -3,5 +3,3 @@ document.querySelector('.dropbtn').addEventListener('click', function() {
     var dropdownContent = document.querySelector('.dropdown-content');
     dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
 });
-
-// test
