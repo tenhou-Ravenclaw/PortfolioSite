2b53676c5a1cbd753d4cc0f4e1aadf0d6af4def2
Fujita Ayato <tenhou.0126@gmail.com>
2025-03-27 11:17:50 +0900

    リンクを別タブで開くように設定

--- src/home/contact.php
+++ src/home/contact.php
@@ -10,7 +10,7 @@ include '../includes/db.php';
 echo '<h1>お問い合わせ</h1>';
 
 // このアドレスからアンケートに飛ぶようにする
-echo '<p>お問い合わせは<a href="https://docs.google.com/forms/d/e/1FAIpQLSeKLj-bnw1KYSRaDFFtJUsxJjD6c6JnkJQf_qIJrOVk2DSydA/viewform?usp=dialog">こちら</a>からお願いします。</p>';
+echo '<p>お問い合わせは<a href="https://docs.google.com/forms/d/e/1FAIpQLSeKLj-bnw1KYSRaDFFtJUsxJjD6c6JnkJQf_qIJrOVk2DSydA/viewform?usp=dialog" target="_blank">こちら</a>からお願いします。</p>';
 
 // フッターのインクルード
 include '../includes/footer.php';
